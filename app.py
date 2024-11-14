748dd60bc4360eeecc6a6dbeefc749c34a53a999
Anand <ad3810242@gmail.com>
2024-11-15 04:03:00 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import joblib
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -35,5 +36,9 @@ def predict():
     except Exception as e:
         return f"Error during prediction: {e}"
 
+
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
